65fccce77d1e616648ffb22d49dba9288bbc5698
DESKTOP-A4PH79H\Shepherd <HuangX0206@GMail.com>
2018-06-15 23:38:04 -0400

    Conversion error

--- darknet_bridges/src/ObjectDetection.cpp
+++ darknet_bridges/src/ObjectDetection.cpp
@@ -7,18 +7,19 @@
 #include <sensor_msgs/CameraInfo.h>
 #include <geometry_msgs/PoseStamped.h>
 
-#include "msg/BearingArray.h"
-#include "msg/ObjectDetection.h"
+#include "darknet_ros_msgs/BoundingBoxes.h"
+#include "phd_msgs/BearingArray.h"
+
 
 //=============================================================================\\
 
 //Beginning of Initializaiton 
 
 //Camera info
-double fx = -1;
-double fy = -1;
-double cx = -1;
-double cy = -1;
+double fx=-1;
+double fy=-1;
+double cx=-1;
+double cy=-1;
 double min_rng = 0.2;
 double max_rng = 15;
 double min_ang = 0;
@@ -26,12 +27,16 @@ double max_ang = 0;
 uint32_t width;
 
 //ObjectDetection
+phd_msgs::BearingArray  bearing_array;
 std::string Class;
-double score;
-uint16_t left;
-uint16_t top;
-uint16_t right;
-uint16_t bottom;
+double probability = 0;
+int64_t xmin=0;
+int64_t ymin=0;
+int64_t xmax=0;
+int64_t ymax=0;
+
+
+
 
 //End of Initializaiton
 
@@ -40,18 +45,18 @@ uint16_t bottom;
 //Beginning of Callback functions 
 
 //Calibration Parameters from sensor_msgs/CameraInfo.msg
-void camera_info_Callback(const sensor_msgs::CameraInfoConstPtr& msg)
+void camera_info_Callback(const sensor_msgs::CameraInfo::ConstPtr& msg)
 {	
 	//Image Dimension width
 	width = msg->width;
 
 	//focal lengths 
-	fx = msg->k[0];
-	fy = msg->k[4];
+	fx = msg->K[0];
+	fy = msg->K[4];
 
 	//principle points
-	cx = msg->k[2];
-	cy = msg->k[5];
+	cx = msg->K[2];
+	cy = msg->K[5];
 
 	//minimum angle
 	min_ang = std::atan((cx - width) / fx);
@@ -61,14 +66,15 @@ void camera_info_Callback(const sensor_msgs::CameraInfoConstPtr& msg)
 
 //Object Detection from 
 
-void Object_detection_Callback(const darknet_ros::Boundingbox::ConstPtr& msg)
+void Boundingboxes_Callback(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
 {
-	Class = msg->Class;
-	score = msg->bounding_boxes[0].probability;
-	left = msg->bounding_boxes[1].left;
-	top = msg->bounding_boxes[2].top;
-	right = msg->bounding_boxes[3].right;
-	bottom = msg->bounding_boxes[4].bottom;
+
+	Class = msg->bounding_boxes[0];
+	probability = msg->bounding_boxes[1];
+	xmin = msg->bounding_boxes[2];
+	ymin = msg->bounding_boxes[3];
+	xmax = msg->bounding_boxes[4];
+	ymax = msg->bounding_boxes[5];
 
 }
 
@@ -80,33 +86,35 @@ void Object_detection_Callback(const darknet_ros::Boundingbox::ConstPtr& msg)
 
 //main function
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
-	ros::init(argc, argv, "detection_convereter");
+	ros::init(argc, argv, "detection_converter");
 	ros::NodeHandle nh;
 
-	ros::subscriber camera_sub = nh.subscribe("camera_info", 1, camera_info_Callback);
-	ros::subscriber det_sub = nh.subscribe("Object_detections", 1, Object_detection_Callback);
+	ros::Subscriber camera_sub = nh.subscribe("/camera_info", 1, &camera_info_Callback);
+	ros::Subscriber det_sub = nh.subscribe("/boundingboxes", 1, &Boundingboxes_Callback);
 
-	//Object Detection Conversion
-	darknet_bridge::BearingArray bearing_array;
 
-	bearing_array.array[0].min_range = min_rng;
-	bearing_array.array[1].max_range = max_rng;
-	bearing_array.array[2].min_bearing = min_ang;
-	bearing_array.array[3].max_bearing = max_ang;
-	bearing_array.array[4].Class = Class;
-	bearing_array.array[5].score = score;
-	bearing_array.array[6].left = left;
-	bearing_array.array[7].top = top;
-	bearing_array.array[8].right = right;
-	bearing_array.array[9].bottom = bottom;
+	bearing_array.array[1].min_range = min_rng;
+	bearing_array.array[2].max_range = max_rng;
+	bearing_array.array[3].min_bearing = min_ang;
+	bearing_array.array[4].max_bearing = max_ang;
+	bearing_array.array[6].Class = Class;
+	bearing_array.array[7].probability = probability;
+	bearing_array.array[8].xmin = xmin;
+	bearing_array.array[9].xmax = xmax;
+	bearing_array.array[10].xmax = xmax;
+	bearing_array.array[11].ymax = ymax;
+
+
+
+
 
 	//Publish Bearing array
-	Ros::publisher bearing_pub;
+	ros::Publisher bearing_pub;
 	bearing_pub.publish(bearing_array);
 
-	ROS_INFO("Convertin object detections to bearings...");
+	ROS_INFO("Converting object detections to bearings...");
 	ros::spin();
 	return 0;
 
