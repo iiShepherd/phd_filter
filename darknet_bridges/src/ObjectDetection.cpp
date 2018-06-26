3a89e881b5c05d2b7fb2ba5a34a9c2fd46156627
Eric's-PC <HuangX0206@Gmail.com>
2018-06-26 16:16:04 -0400

    Reorganized the code

--- darknet_bridges/src/ObjectDetection.cpp
+++ darknet_bridges/src/ObjectDetection.cpp
@@ -1,132 +1,104 @@
 #include <cmath>
 #include <stdint.h>
-#include <string>
 #include <iostream>
-
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <geometry_msgs/PoseStamped.h>
-
+#include <sensor_msgs/Camerainfo.h>
 #include "darknet_ros_msgs/BoundingBoxes.h"
 #include "phd_msgs/BearingArray.h"
 
+// Struct a custom Class
 
-//=============================================================================\\
-//Beginning of Initializaiton 
-
-//Camera info
-double fx=-1;
-double fy=-1;
-double cx=-1;
-double cy=-1;
-double min_rng = 0.2;
-double max_rng = 15;
-double min_ang = 0;
-double max_ang = 0;
-uint32_t width = 0;
-
-geometry_msgs::PoseStamped currPose;
-
-//ObjectDetection
-std::string Class;
-double probability = 0;
-int64_t xmin=0;
-int64_t ymin=0;
-int64_t xmax=0;
-int64_t ymax=0;
-
-
-
-
-//End of Initializaiton
-
-//=============================================================================\\
-
-//Beginning of Callback functions 
-
-//Calibration Parameters from sensor_msgs/CameraInfo.msg
-void camera_info_Callback(const sensor_msgs::CameraInfo::ConstPtr& msg)
-{	
-	//Image Dimension width
-	width = msg->width;
-
-	//focal lengths 
-	fx = msg->K[0];
-	fy = msg->K[4];
-
-	//principle points
-	cx = msg->K[2];
-	cy = msg->K[5];
-
-	//minimum angle
-	min_ang = std::atan((cx - width) / fx);
-	max_ang = std::atan((cx - 1 / fx));
-
-
-}
-
-//Object Detection from 
-
-void Boundingboxes_Callback(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
+struct var
 {
+	//BoundingBox Param
+	std::string Class;
+	double probability;
+	int64_t xmin;
+	int64_t xmax;
+	int64_t ymin;
+	int64_t ymax;
 
-	Class = msg->bounding_boxes[0].Class;
-	probability = msg->bounding_boxes[0].probability;
-	xmin = msg->bounding_boxes[0].xmin;
-	ymin = msg->bounding_boxes[0].ymin;
-	xmax = msg->bounding_boxes[0].xmax;
-	ymax = msg->bounding_boxes[0].ymax;
-
-
-
-}
-
-
-
-//End of Callback functions 
-
-//=============================================================================\\
-
-//main function
-
-int main(int argc, char **argv)
-{
-	ros::init(argc, argv, "detection_converter");
-	ros::NodeHandle nh;
-
-	ros::Subscriber camera_sub = nh.subscribe("/usb_cam/camera_info", 1, &camera_info_Callback);
-	ros::Subscriber det_sub = nh.subscribe("/darknet_ros/bounding_boxes", 1, &Boundingboxes_Callback);
-
-	ROS_INFO("Converting object detections to bearings...");
-
-	while(ros::ok)
-	{
-	phd_msgs::BearingArray  bearing_array;
+	//Camerainfo Param
 	std::string child_frame_id;
-	bearing_array.child_frame_id = child_frame_id;
-	bearing_array.pose = currPose;
-	
-	bearing_array.array.resize(1);									// array  needs to be dynamic
-	bearing_array.array[0].min_range = min_rng;
-	bearing_array.array[0].max_range = max_rng;
-	bearing_array.array[0].min_bearing = min_ang;
-	bearing_array.array[0].max_bearing = max_ang;
-	bearing_array.array[0].Class = Class;
-	bearing_array.array[0].probability = probability;
-	bearing_array.array[0].xmin = xmin;
-	bearing_array.array[0].xmax = xmax;
-	bearing_array.array[0].ymin = ymin;
-	bearing_array.array[0].ymax = ymax;
+	double fx;
+	double fy;
+	double cx;
+	double cy;
+	double min_rng;
+	double max_rng;
+	double min_ang;
+	double max_ang;
+	uint32_t width;
 
-	//Publish Bearing array
-	ros::Publisher bearing_pub = nh.advertise<phd_msgs::BearingArray>("/bridge/measurement", 100);
+	//ROS
+	ros::NodeHandle nh;
+	ros::publisher bearing_pub;
 
-	bearing_pub.publish(bearing_array);
-	ros::spinOnce();
+	//array num_det
+	phd_msgs::BearingArray bearing_array;
+	unsigned int num_det;
+}param;
+
+	//callback functions
+void camera_info_callback(const sensor_msgs::Camerainfo::COnstPtr& msg)
+{
+	param.child_frame_id = msg->header.frame_id;
+	param.width = msg->width;
+	//focal length
+	param.fx = msg->K[0];
+	param.fy = msg->K[4];
+	//principle points
+	param.cx = msg->K[2];
+	param.cy = msg->K[5];
+	//min/max angle
+	param.min_ang = std::atan((cx - width) / fx);
+	param.max_ang = std::atan((cx - 1) / fx);
+
+
+}
+void boundingboxes_callback(const dakrnet_ros_msgs::BoundingBoxes::ConstPtr& msg)
+{
+	param.Class = msg->bounding_boxes[0].Class;
+	param.probability = msg->bounding_boxes[0].probability;
+	param.xmin = msg->bounding_boxes[0].xmin;
+	param.ymin = msg->bounding_boxes[0].ymin;
+	param.xmax = msg->bounding_boxes[0].xmax;
+	param.ymax = msg->bounding_boxes[0].ymax;
+
+	param.num_det = msg->bounding_boxes.size();
+	bearing_array.array.resize(num_det);
+	for (unsigned int k = 0; k < num_det; ++k)
+	{
+		double centerx = (xmax + xmin) / 2;
+		bearing_array.array[k].bearing = std::atan((param.cx - centerx) / param.fx);
+		bearing_array.array[k].min_range = param.min_rng;
+		bearing_array.array[k].max_range = param.max_rng;
+		bearing_array.array[k].min_bearing = param.min_ang;
+		bearing_array.array[k].max_bearing = param.max_ang;
+		bearing_array.array[k].Class = param.Class;
+		bearing_array.array[k].xmin = param.xmin;
+		bearing_array.array[k].xmax = param.xmax;
+		bearing_array.array[k].ymin = param.ymin;
+		bearing_array.array[k].ymax = param.ymax;
 	}
 
- return 0;
+	param.bearing_pub.publish(bearing_array);
+
+
+
 }
 
+int main(int argc, char** argv)
+{
+	//initiattion
+	ros::init(argc, argv, "detection_converter");
+	param.bearing_pub = param.nh.advertise <phd_msgs::BearingArray>("/bridge/measurement", 100);
+	//subscriptions (refer to rostopic list)
+	ros::Subscriber camera_sub = param.nh.subscribe("/usb_cam/camera_info", 1, &camera_info_callback);
+	ros::Subscriber pose_sub = param.nh.subscribe("/pose", 1, &pose_callback);
+	ros::Subscriber det_sub = param.nh.subscribe("/darknet_ros/bounding_boxes", 1, &boundingboxes_callback);
 
+	ros::spinOnce();
+	return 0;
+}
